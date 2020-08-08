f42068314602cb6b2585b7740d7229178796296a
fran2203 <franciscoutrera@live.com>
2020-08-08 02:04:31 -0300

    Comentarios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-//importo express(framework de Nodejs orientado a la creacion de paginas webs y APIs)
+//Importo express(framework de Nodejs orientado a la creacion de paginas webs y APIs)
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Servidor iniciado')
+app.set('port', process.env.PORT || 3000); /*Setea el puerto al que te da el servidor donde desplegamos el proyecto, si no te da uno, se utiliza el puerto 3000 por defecto*/
+
+//Inicializa el server
+app.listen(app.get('port'), () => {
+    console.log(`Servidor iniciado en el puerto ${app.get('port')}`);
 })
