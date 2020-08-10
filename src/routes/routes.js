95630b66ca5c269f4ffa022a99ebecf69ddfc2fe
fran2203 <franciscoutrera@live.com>
2020-08-10 01:40:08 -0300

    nuevas rutas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,10 +6,38 @@ router.get('/', (req, res) => {     //Configuramos que hacer cuando hay un GET a
     res.render('index');
 })
 
+router.get('/loggin', (req, res) => {
+    res.send('loggin');
+})
+
+router.get('/admin', (req, res) => {
+    res.send('Administrar');
+})
+
+router.get('/admin:id', (req, res) => {
+    res.send('Editar comida');
+})
+
+router.post('/admin:id', (req, res) => {
+    res.send('Editar comida');
+})
+
+router.get('/agregar', (req, res) => {
+    res.send('agregar comida');
+})
+
 router.post('/agregar', async (req, res) => {
     const comida = new Comida(req.body);
     await comida.save();
     res.send('recibido');
+})
+
+router.get('/comprar', (req, res) => {
+    res.send('Comprar');
+})
+
+router.post('/comprar', (req, res) => {
+    res.send('Compra realizada');
 })
 
 module.exports = router;            //La exporto para poder utilizarla en los demas archivos donde se llama este archivo (Index.js)
